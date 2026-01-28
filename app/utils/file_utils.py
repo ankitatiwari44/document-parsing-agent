d4979fa029e9e15a18fd19f8ebe645f53541a591
ankitatiwari44 <t.ankita0110@gmail.com>
2026-01-28 20:58:36 +0530

    input processsing

--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -1,5 +1,17 @@
-from PIL import Image
 import io
+import fitz  # PyMuPDF
+from PIL import Image
 
-def load_image(file_bytes: bytes) -> Image.Image:
+def load_image_from_bytes(file_bytes: bytes) -> Image.Image:
     return Image.open(io.BytesIO(file_bytes)).convert("RGB")
+
+def pdf_to_images(pdf_bytes: bytes):
+    images = []
+    pdf = fitz.open(stream=pdf_bytes, filetype="pdf")
+
+    for page in pdf:
+        pix = page.get_pixmap(matrix=fitz.Matrix(2, 2))
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        images.append(img)
+
+    return images
